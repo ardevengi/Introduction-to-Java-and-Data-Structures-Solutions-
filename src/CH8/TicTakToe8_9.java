a22d9bc72d00f6a3e60bd6d26fc4716f99c1cc80
ardevengi <ardevengi@gmail.com>
2023-09-22 19:01:48 +0300

    xo

--- src/CH8/TicTakToe8_9.java
+++ src/CH8/TicTakToe8_9.java
@@ -13,7 +13,8 @@ public class TicTakToe8_9 {
         Scanner iScanner = new Scanner(System.in);
         int i, j;
         sampleBoard();
-        while (full(grid) || !(won(grid, 'X') || won(grid, 'Y'))) {
+
+        do {
             System.out.println("Enter a row (0 , 1 , 2) for player " + player);
             System.out.println("Enter a col (0 , 1 , 2) for players " + player);
             i = iScanner.nextInt();
@@ -21,13 +22,13 @@ public class TicTakToe8_9 {
             grid[i][j] = player;
             printBoard(grid);
             player = player == 'X' ? 'Y' : 'X';
-        }
+        } while (!full(grid) && !won(grid));
     }
 
-    public static boolean won(char grid[][], char player) {
+    public static boolean won(char grid[][]) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (won(grid, i, j, player))
+                if (won(grid, i, j))
                     return true;
 
             }
@@ -35,15 +36,61 @@ public class TicTakToe8_9 {
         return false;
     }
 
-    public static boolean won(char grid[][], int i, int j, char player) {
-        for (int r = j + 1; r < j + 4 && r < grid[0].length; r++) {
-            if (!(grid[i][j] == grid[i][r]=+player))
-                break;
-            else {
-                count++;
+    public static boolean won(char grid[][], int i, int j) {
+        if (grid[i][j] != ' ') {
+            int count = 1;
+            for (int r = j + 1; r < j + 3 && r < grid[0].length; r++) {
+                if (!(grid[i][j] == grid[i][r]) && (grid[i][j] != ' '))
+                    break;
+                else {
+                    count++;
+                }
+            }
+
+            if (count == 3) {
+                System.out.println("the winner is horizontal " + grid[i][j]);
+                return true;
+            }
+            count = 1;
+
+            for (int c = i + 1; c < i + 3 && c < grid.length; c++) {
+                if (!(grid[i][j] == grid[c][j]) && (grid[i][j] != ' '))
+                    break;
+                else {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                System.out.println("the winner is vertical" + grid[i][j]);
+                return true;
+            }
+            count = 1;
+
+            for (int c = i + 1, r = j - 1; c < i + 3 && c < grid.length && r > j - 3 && r >= 0; r--, c++) {
+                if (!(grid[i][j] == grid[c][r]) && (grid[i][j] != ' '))
+                    break;
+                else {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                System.out.println("the winner is l diagonal" + grid[i][j]);
+                return true;
+            }
+            count = 1;
+            for (int c = i + 1, r = j + 1; c < i + 3 && c < grid.length && r < j + 3 && r < grid[0].length; r++, c++) {
+                if (!(grid[i][j] == grid[c][r]) && (grid[i][j] != ' '))
+                    break;
+                else {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                System.out.println("the winner is r diagonal" + grid[i][j]);
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     public static boolean full(char grid[][]) {
@@ -54,7 +101,7 @@ public class TicTakToe8_9 {
                     count++;
             }
         }
-        return count < 9;
+        return count > 9;
     }
 
     public static void printBoard(char grid[][]) {
